53c1b9c31561006fbcc12fe2c2ed0f7fe4703d95
menna <menna.mohamed.omar@gmail.com>
2021-04-17 01:04:07 +0200

    create navbar

---
+++ src/components/heroSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeroSection = () => {
+    return ( 
+        <React.Fragment>
+            <h1>hero</h1>
+        </React.Fragment>
+    );
+}
+ 
+export default HeroSection;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,81 @@
 import React from "react";
 
-// import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    return ( 
-        <React.Fragment>
-            <h1 className="b">Nav bar</h1>
-        </React.Fragment>
-    );
-}
- 
+  return (
+    <React.Fragment>
+      <nav className="navBar navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <img
+              className="navBar__image"
+              src="./images/logo.png"
+              alt="logo"
+            ></img>
+            <span className="navBar__span">Blogs</span>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="navBar__middle collapse navbar-collapse"
+            id="navbarSupportedContent"
+            style={{ flexDirection: "column" }}
+          >
+            <ul className="navBar__list navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="navBar__list__item nav-item">
+                <NavLink
+                  className="navBar__list__item__link navBar__list__item__link--active nav-link"
+                  aria-current="page"
+                  to="/home"
+                >
+                  HOME
+                </NavLink>
+              </li>
+              <li className="navBar__list__item nav-item">
+                <NavLink
+                  className="navBar__list__item__link nav-link"
+                  to="/about"
+                >
+                  ABOUT
+                </NavLink>
+              </li>
+              <li className="navBar__list__item nav-item">
+                <NavLink
+                  className="navBar__list__item__link nav-link"
+                  to="/blog"
+                >
+                  BLOGS
+                </NavLink>
+              </li>
+              <li className="navBar__list__item nav-item">
+                <NavLink
+                  className="navBar__list__item__link nav-link"
+                  to="/contactUs"
+                >
+                  CONTACT US
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <button className="navBar__button btn">
+            <NavLink className="navBar__button__link nav-link" to="/login">
+              LOGIN
+            </NavLink>
+          </button>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+};
+
 export default Navbar;
